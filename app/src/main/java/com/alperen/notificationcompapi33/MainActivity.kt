c721393f6bf6a355fd3a1af9735886bd7caa3e63
AlpErenKaya0 <80575610+AlpErenKaya0@users.noreply.github.com>
2024-02-27 13:59:52 +0300

    init

--- app/src/main/java/com/alperen/notificationcompapi33/MainActivity.kt
+++ app/src/main/java/com/alperen/notificationcompapi33/MainActivity.kt
@@ -1,15 +1,35 @@
 package com.alperen.notificationcompapi33
 
+import android.Manifest
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.alperen.notificationcompapi33.ui.theme.NotificationCompApi33Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +37,61 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NotificationCompApi33Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val context = LocalContext.current
+                var hasNotificationPermission by remember {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        mutableStateOf(
+                            ContextCompat.checkSelfPermission(
+                                context,
+                                Manifest.permission.POST_NOTIFICATIONS
+                            ) == PackageManager.PERMISSION_GRANTED
+                        )
+                    } else {
+                        mutableStateOf(true)
+                    }
                 }
+                val permissionLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission(),
+                    onResult = {isGranted ->
+                        hasNotificationPermission = isGranted
+                    if (isGranted){
+                        //shouldShowRequestPermissionRationale()
+                    }
+                    }
+                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalArrangement =Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick =  {
+                       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                           permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                       }   }) {
+                        Text(text = "Request Permission")
+                    }
+                    Button(onClick = {
+                        if (hasNotificationPermission){
+                        showNotification()}
+                    }) {
+                        Text(text = "Show Notification")
+                        
+                    }
+                }
+
+
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotificationCompApi33Theme {
-        Greeting("Android")
+    private fun showNotification(){
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notification = NotificationCompat.Builder(applicationContext,"channel_id")
+            .setContentText("This is some content")
+            .setContentTitle("hello world")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .build()
+        notificationManager.notify(1,notification)
     }
 }
